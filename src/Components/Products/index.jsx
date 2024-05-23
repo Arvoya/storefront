77fcc405856cff9004cbd721e8f5731b05fb6b8b
Isai Chaidez <isaichaidez@gmail.com>
2024-05-22 18:42:52 -0700

    fixed cart, issue with backend store not having enough products

--- src/Components/Products/index.jsx
+++ src/Components/Products/index.jsx
@@ -19,18 +19,35 @@ import { fetchProducts } from '../../store/products';
 export default function Products() {
   const dispatch = useDispatch();
 
-  const filteredProducts = useSelector(state => state.product.filteredProducts)
+  let filteredProducts = useSelector(state => state.product.filteredProducts)
+  const categories = useSelector(state => state.category.categories);
+  const categoryNames = categories.map(category => category.name)
 
   const handleCart = (product) => {
-    console.log('here is the product', product)
     dispatch(addToCart(product));
   }
 
+  const filterProducts = (products, categories) => {
+    let categoryFiltered = products.filter(product =>
+      categories.includes(product.category)
+    )
+
+
+    //NOTE: If we run this code, it will filter out all products as none of the categorized products have a stock greater than 0
+    // let finalProducts = categoryFiltered.filter(product =>
+    //   product.inStock > 0
+    // )
+    // return finalProducts;
+
+    return categoryFiltered;
+
+  }
+
+  filteredProducts = filterProducts(filteredProducts, categoryNames)
 
 
   useEffect(() => {
     dispatch(fetchProducts());
-    console.log(filteredProducts)
   }, [])
 
   return (
@@ -43,7 +60,7 @@ export default function Products() {
           '& > :not(style)': {
             m: 1,
             width: 420,
-            height: 420,
+            height: 220,
           },
         }}
       >
@@ -60,11 +77,12 @@ export default function Products() {
             />
             <CardMedia
               component="img"
-              height="194"
+              height="10"
               sx={{ objectFit: 'contain' }}
               image={product.image}
               alt={product.name}
             />
+
             <CardContent>
               <Typography variant="body2" color="text.secondary">
                 {product.description}
--- src/store/cart.js
+++ src/store/cart.js
@@ -5,13 +5,54 @@ const initialState = {
 export const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD":
-      return {
-        products: [...state.products, action.payload],
-      };
-    case "REMOVE":
-      return {
-        products: state.products.filter((product) => product != action.payload),
+      // Check if the product already exists in the cart
+      const existingProductIndex = state.products.findIndex(
+        (item) => item._id === action.payload._id,
+      );
+
+      // If it exists, increase its quantity
+      if (existingProductIndex >= 0) {
+        const updatedProducts = state.products.map((item, index) =>
+          index === existingProductIndex
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        );
+        return { products: updatedProducts };
       }
+
+      // If it doesn't exist, add it with a quantity of 1
+      return {
+        products: [...state.products, { ...action.payload, quantity: 1 }],
+      };
+
+    case "REMOVE":
+      // Find the product in the cart
+      const productIndex = state.products.findIndex(
+        (item) => item._id === action.payload._id,
+      );
+
+      // If the product is found and its quantity is greater than 1, decrease the quantity
+      if (productIndex >= 0) {
+        const updatedProducts = state.products.map((item, index) =>
+          index === productIndex
+            ? { ...item, quantity: item.quantity - 1 }
+            : item,
+        );
+
+        // If the quantity becomes 0, remove the product from the cart
+        if (updatedProducts[productIndex].quantity === 0) {
+          return {
+            products: updatedProducts.filter(
+              (item) => item._id !== action.payload._id,
+            ),
+          };
+        }
+
+        return { products: updatedProducts };
+      }
+
+      return state;
+
     default:
       return state;
   }
@@ -30,4 +71,3 @@ export const removeFromCart = (product) => {
     payload: product,
   };
 };
-
